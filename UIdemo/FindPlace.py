480847481bf0708c8e8b5c327a432196b68b20c4
Leo <leowangx2013@outlook.com>
2015-03-15 01:02:17 +0800

    A Demo :)

--- UIdemo/FindPlace.py
+++ UIdemo/FindPlace.py
@@ -1,3 +1,7 @@
+'''
+	This is a program allowing users to search a particular place in China
+	by inputing certain conditions :)
+'''
 import pymongo
 from pymongo import MongoClient
 import bottle
@@ -7,37 +11,72 @@ db = connection.weather
 
 conditions = {}
 
-def search_in_db():
-	global conditions
-	print(conditions)
-	items = db.conditions.find({'ELEVATION':conditions['elevation'], 'LATITUDE':conditions['latitude'], 
-		'LONGITUDE':conditions['longitude'], 'DATE':conditions['date'], 'PRCP':conditions['precipitation']})
-	for item in items:
-		print(item)
-
+# Initialize the home page
 @bottle.route('/')
 def make_home_page():
 	return bottle.template('home_page')
 
+# Called after user press the button
+# Get the given conditions from text boxes in the homepage
 @bottle.post('/')
 def process_search():
 	global conditions
-	conditions['username'] = bottle.request.forms.get('username')
-	conditions['elevation'] = bottle.request.forms.get('elevation')
-	conditions['latitude'] = bottle.request.forms.get('latitude')
-	conditions['longitude'] = bottle.request.forms.get('longitude')
-	conditions['date'] = bottle.request.forms.get('date')
-	conditions['precipitation'] = bottle.request.forms.get('precipitation')
-	conditions['tmax'] = bottle.request.forms.get('tmax')
-	conditions['tmini'] = bottle.request.forms.get('tmini')
+	print('flag', bottle.request.forms.get('elevation'))
+	if bottle.request.forms.get('elevation') != '':
+		conditions['elevation'] = float(bottle.request.forms.get('elevation'))
+	if bottle.request.forms.get('latitude') != '':
+		conditions['latitude'] = float(bottle.request.forms.get('latitude'))
+	if bottle.request.forms.get('longitude') != '':
+		conditions['longitude'] = float(bottle.request.forms.get('longitude'))
+	if bottle.request.forms.get('precipitation') != '':
+		conditions['precipitation'] = float(bottle.request.forms.get('precipitation'))
+	if bottle.request.forms.get('snowdepth') != '':
+		conditions['snowdepth'] = float(bottle.request.forms.get('snowdepth'))
+	if bottle.request.forms.get('tmax') != '':
+		conditions['tmax'] = float(bottle.request.forms.get('tmax'))
+	if bottle.request.forms.get('tmin') != '':
+		conditions['tmin'] = float(bottle.request.forms.get('tmin'))
 	
 	print(conditions)
 	bottle.redirect("/search")
 
+# Use the given conditions to search in the database
+def search_in_db():
+	global conditions
+	# for item in conditions:
+	# 	if conditions[item] == '':
+	# 		conditions.pop(item)
+	conditions = modify_dic(conditions)
+	items = db.condition.find(
+		{
+
+		})
+	print('items=', items)
+	return items
+
+def modify_dic(conditions):
+	new_conditions = {}
+	if 'elevation' in conditions:
+		new_conditions['ELEVATION'] = conditions['elevation']
+	if 'latitude' in conditions:
+		new_conditions['LATITUDE'] = conditions['latitude']
+	if 'longitude' in conditions:
+		new_conditions['LONGITUDE'] = conditions['longitude']
+	if 'precipitation' in conditions:
+		new_conditions['PRCP'] = conditions['precipitation']
+	if 'snowdepth' in conditions:
+		new_conditions['SNWD'] = conditions['snowdepth']
+	if 'tmax' in conditions:
+		new_conditions['TMAX'] = conditions['tmax']
+	if 'tmin' in conditions:
+		new_conditions['TMIN'] = conditions['tmin']
+	return new_conditions
+
+
+# Display the search result
 @bottle.route('/search')
 def show_result():
-	search_in_db()
-	return bottle.template('result')
+	return bottle.template('result', {"Items": search_in_db()})
 
 
 bottle.debug(True)
--- UIdemo/result.tpl
+++ UIdemo/result.tpl
@@ -1,9 +1,12 @@
 <html>
-
-<body background="images/background.jpg">
-<br>
-<center>Result</center>
-
+<head>
+<title>Result</title>
+</head>
+<body>
+<ul>
+%for item in Items:
+<li>{{item['STATION_NAME']}} {{item['DATE']}}</li>
+%end
 </ul>
 </body>
 </html>
